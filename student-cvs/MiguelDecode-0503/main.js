310303a799d0a828f55ee32143c3d9e7916ffac4
Miguel Decode <migueldecode@gmail.com>
2023-11-16 15:09:21 +0100

    feat: create a responsive cv

---
+++ student-cvs/MiguelDecode-0503/main.js
@@ -0,0 +1,8 @@
+// main.js
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log("Hola Mundo");
+}
+
+const $form = document.querySelector("form");
+$form.addEventListener("submit", handleSubmit);
